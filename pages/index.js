8b9d67c3e308bc1ec09030f76773431819bf50b8
thelonehegelian <thelonehegelian@gmail.com>
2023-02-10 07:24:36 +0000

    Fix response text area

--- pages/index.js
+++ pages/index.js
@@ -68,34 +68,19 @@ const Home = () => {
           </a>
         </div>
 
-        <div className="prompt-container">
-          <textarea
-            placeholder="start typing here"
-            className="prompt-box"
-            value={userInput}
-            onChange={onUserChangedText}
-          />
-          <div className="prompt-buttons">
-            <a className="generate-button" onClick={callGenerateEndpoint}>
-              <div className="generate">
-                <p>Generate</p>
-              </div>
-            </a>
-          </div>
-          {/* New code I added here */}
-          {apiOutput && (
-            <div className="output">
-              <div className="output-header-container">
-                <div className="output-header">
-                  <h3>Output</h3>
-                </div>
-              </div>
-              <div className="output-content">
-                <p>{apiOutput}</p>
+        {/* Reponse text */}
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
               </div>
             </div>
-          )}
-        </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
