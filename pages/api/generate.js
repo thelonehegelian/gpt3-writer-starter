bbc51e505401491b13ab7f1d9006976ec1a106c6
thelonehegelian <thelonehegelian@gmail.com>
2023-02-11 09:31:51 +0000

    Chain second prompt
    Coming up undefined

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,37 +6,40 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-let secondPrompt = '';
-
-const basePromptPrefix = `Explain concepts using various characters from Simpsons, make it like as if they are having a conversation, make it funny and memorable and use each characters catch phrases.
-Explain the concept: `;
-// Run first prompt
+const basePromptPrefix = `Explain concepts using various characters from Simpsons, make it like as if they are having a conversation, make it funny and memorable and use each characters catch phrase.
+Explain: `;
 const generateAction = async (req, res) => {
-  // console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
-  secondPrompt = `Explain ${req.body.userInput} using various characters from South Park, make it like as if they are having a conversation. Be bold and ridiculous`;
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
     temperature: 0.87,
-    max_tokens: 250,
+    max_tokens: 100,
   });
-
   const basePromptOutput = baseCompletion.data.choices.pop();
-
+  const secondPrompt = `${basePromptOutput.text.slice(
+    0,
+    50
+  )}. Eric Cartman, from South Park, shuts everyone up and says ${
+    req.body.userInput
+  } is ...`;
   const secondPromptCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     // don't want to use previous completion as a prompt, too many tokens used
     // @todo use the last line of the previous completion as part of the prompt
-    // example: `Continue with South Park characters: ${basePromptOutput.text}`
+    // example: `Continue with South Park characters: ${basePromptOutput.text}. Cartman shuts everyone and says...`
+    // slice off 50 words from the end of the previous completion
+
     prompt: `${secondPrompt}`,
     // increase the temperature to make the output more creative
     temperature: 0.95,
-    max_tokens: 250,
+    max_tokens: 100,
   });
-
   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  res.status(200).json({ output: basePromptOutput + secondPromptOutput });
 };
 
 export default generateAction;
